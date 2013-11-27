38c987676cc760de6eb89d665f70bf50af1c8be1
akelisakieva <andriana.kelisakieva@volarvideo.com>
2013-11-26 22:41:38 -0500

    Create InputStreamReader.java

---
+++ InputStreamReader.java
@@ -0,0 +1,10 @@
+import java.io.InputStream;
+
+
+public class InputStreamReader {
+
+        public InputStreamReader(InputStream in) {
+                // TODO Auto-generated constructor stub
+        }
+
+}
